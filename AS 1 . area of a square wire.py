b537d11f805d554afce447dc9a1d819a04cbcd56
priyanshi-vish22 <vishwakarmapriyanshi545@gmail.com>
2025-11-20 09:57:06 +0530

    Add files via upload

---
+++ AS 1 . area of a square wire.py
@@ -0,0 +1,9 @@
+import math
+angle = 60
+radius = 42
+arc_length = (angle / 360) * (2 * math.pi * radius)
+side = arc_length /4
+area = side ** 2
+print("Arc Length:", arc_length)
+print("Side of Square:", side)
+print("Area of Square:", area)
